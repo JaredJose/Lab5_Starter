ee4c3affe702e634821e04d6998a47528622eff7
JaredJose <jjose@ucsd.edu>
2023-05-03 17:33:17 -0700

    beginning explore section

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,19 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let voices = speechSynthesis.getVoices();
+
+  console.log(speechSynthesis.getVoices().length);
+  console.log("THE VOICES");
+  console.log(voices.length);
+  for(let i = 0; i < voices.length; i++){
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    document.getElementById("voice-select").appendChild(option);
+  }
+  console.log("DONE WITH THE VOICES");
 }
+
